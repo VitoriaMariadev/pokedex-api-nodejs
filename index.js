7a2a57685c5f25955840d14437d3b3e15ed2b790
VitoriaMariadev <vitoriamaria.rodrigues02@gmail.com>
2023-06-24 23:54:25 -0300

    FIX/ Erro no cors

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ import Route from './src/routes/routes.js';
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.use(cors({
   origin: '*', // Coloque o endereço do seu aplicativo aqui
   // Outras opções de configuração...
