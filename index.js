c90986439344f63a9b059a43667eefe957f128b5
VitoriaMariadev <vitoriamaria.rodrigues02@gmail.com>
2023-06-24 23:58:26 -0300

    FIX/ Erros

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import Route from './src/routes/routes.js';
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-app.use(cors({
-  origin: '*', // Coloque o endereço do seu aplicativo aqui
-  // Outras opções de configuração...
-}));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+app.use(cors())
 
+app.use(cookieParser())
 app.use(express.json());
 app.use('/', Route);
 
