888dce1beabffe8b2fcc36351c131d75048d5e53
VitoriaMariadev <vitoriamaria.rodrigues02@gmail.com>
2023-06-25 00:12:37 -0300

    FIX/ Mudanças

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    next();
+  });
 app.use(cors())
 
 app.use(cookieParser())
