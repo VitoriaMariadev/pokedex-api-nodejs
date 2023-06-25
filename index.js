20a1be25f08494681284a285e3b67c9d5d176982
VitoriaMariadev <vitoriamaria.rodrigues02@gmail.com>
2023-06-24 23:45:05 -0300

    FIX/ Erro no cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const port = 3000
 //   extended: true
 // })); 
 app.use(cors({
-    origin: 'http://localhost:3000', // Coloque o endereço do seu aplicativo aqui
+    origin: '*', // Coloque o endereço do seu aplicativo aqui
     // Outras opções de configuração...
   }));
 
